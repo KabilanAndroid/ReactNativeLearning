ea139a7578e5e785ef380db1b812939f4b50724f
KabilanAndroid <kabilan@evapps.in>
2025-10-17 10:20:46 +0530

    redux basic implement

--- KabilanProject/src/redux/slices/counterSlice.tsx
+++ KabilanProject/src/redux/slices/counterSlice.tsx
@@ -1,17 +1,10 @@
-// src/redux/slices/counterSlice.ts
 import { createSlice } from '@reduxjs/toolkit';
 
-interface CounterState {
-  value: number;
-}
-
-const initialState: CounterState = {
-  value: 0,
-};
-
-const counterSlice = createSlice({
+export const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: {
+    value: 0,
+  },
   reducers: {
     increment: (state) => {
       state.value += 1;
@@ -19,12 +12,11 @@ const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
-    reset: (state) => {
-      state.value = 0;
-    },
   },
 });
 
-export const { increment, decrement, reset } = counterSlice.actions;
+export const { increment, decrement } = counterSlice.actions;
+
+export const selectCount = (state) => state.counter.value;
 
 export default counterSlice.reducer;
