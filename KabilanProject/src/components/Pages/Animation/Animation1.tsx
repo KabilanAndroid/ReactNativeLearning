841cb40232cc957c80c9e62f8d10339ace80bb32
KabilanAndroid <kabilan@evapps.in>
2025-10-29 12:47:04 +0530

    complete toggle like animation

--- KabilanProject/src/components/Pages/Animation/Animation1.tsx
+++ KabilanProject/src/components/Pages/Animation/Animation1.tsx
@@ -1,42 +1,73 @@
 import {
   Animated,
+  Image,
   LayoutChangeEvent,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { image } from '../../../utils/Images';
 
 const Animation1 = () => {
   const animatedHeight = useRef(new Animated.Value(0)).current;
+  const RotateAnim = useRef(new Animated.Value(0)).current;
   const [isEnabled, setIsEnabled] = useState(false);
   const [dynheight, setdynheight] = useState(0);
 
   const toggleAnimation = () => {
-    setIsEnabled(_=>!_);
+    setIsEnabled(_ => !_);
     Animated.timing(animatedHeight, {
       toValue: isEnabled ? 0 : dynheight,
       duration: 500,
       useNativeDriver: false,
     }).start();
+
+    
   };
+  useEffect (()=>{
+    Animated.timing(RotateAnim, {
+      toValue: isEnabled ? 0 : 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+},[isEnabled,RotateAnim])
 
   const handleLayout = (event: LayoutChangeEvent) => {
-    
     if (dynheight === 0) {
       setdynheight(event.nativeEvent.layout.height);
     }
   };
-
+  const rotateStyle = RotateAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '180deg'],
+  });
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.clickbutton} onPress={toggleAnimation}>
         <Text style={styles.text}>Press Me</Text>
+        <Animated.Image
+          source={image.uparrow}
+          style={[
+            styles.image,
+
+            {
+              transform: [
+                {
+                  rotate: rotateStyle,
+                },
+              ],
+            },
+          ]}
+        />
+        ;
       </TouchableOpacity>
 
-      
-      <View style={{ opacity: 0, position: 'absolute' }} onLayout={handleLayout}>
+      <View
+        style={{ opacity: 0, position: 'absolute' }}
+        onLayout={handleLayout}
+      >
         <Text style={styles.text}>dhg</Text>
         <Text style={styles.text}>ggdgdhf</Text>
       </View>
@@ -58,21 +89,33 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
   },
   clickbutton: {
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     backgroundColor: 'skyblue',
-    width: '100%',
-    padding: 10,
-    borderRadius: 5,
+    width: '50%',
+    paddingVertical: 6,
+    paddingHorizontal: 20,
+    borderRadius: 14,
+    alignSelf: 'center',
+    marginVertical: 10,
   },
   box: {
     width: '100%',
-    overflow: 'hidden', 
-    backgroundColor: 'white',
+    overflow: 'hidden',
+    backgroundColor: 'skyblue',
   },
   text: {
     fontSize: 16,
     color: 'darkblue',
-    padding: 10,
+  },
+  image: {
+    width: 18,
+    height: 18,
+  },
+  image2: {
+    transform: [{ rotate: '90deg' }],
+    width: 18,
+    height: 18,
   },
   box1: {
     height: '50%',
